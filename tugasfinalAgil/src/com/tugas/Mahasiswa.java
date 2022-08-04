c2963a9731987fe49b7a3436595bea61a87cf455
Agil22116007 <107743192+Agil22116007@users.noreply.github.com>
2022-08-04 18:00:54 +0700

    tugas4

---
+++ tugasfinalAgil/src/com/tugas/Mahasiswa.java
@@ -0,0 +1,32 @@
+package com.tugas;
+
+public class Mahasiswa {
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNIM() {
+        return NIM;
+    }
+
+    public void setNIM(String NIM) {
+        this.NIM = NIM;
+    }
+
+    public double getIPK() {
+        return IPK;
+    }
+
+    public void setIPK(double IPK) {
+        this.IPK = IPK;
+    }
+
+    private String NIM;
+    private double IPK;
+}
